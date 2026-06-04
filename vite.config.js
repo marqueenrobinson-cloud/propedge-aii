8049ab511cb5d8ad1b08ee279067f2c6071e2594
marqueenrobinson-cloud <marqueenrobinson@gmail.com>
2026-06-04 09:13:12 -0700

    Update allowedHosts in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['sb-5j6hmu1aqhs0.vercel.run'],
+    allowedHosts: ['.vercel.run'],
   },
 })
